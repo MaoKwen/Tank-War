be17d1330f414bc45ad18658dfd2434f8f3da52a
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2020-08-17 12:06:41 +0800

    env

---
+++ Assets/Scripts/TankCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCamera : MonoBehaviour
+{
+    public int columnLength, rowLength;
+    public float cellSizeX, cellSizeY;
+    public float spacingX, spacingY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int )
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TankController : MonoBehaviour
+{
+    public const float m_Speed = 12.0f;
+    public const float m_TurnSpeed = 120.0f;
+
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+
+    private Rigidbody m_Rigidbody;
+    private NavMeshAgent m_navMeshAgent;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void OnEnable()
+    {
+        // When the tank is turned on, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_MovementInputValue = Input.GetAxis("Vertical");
+        m_TurnInputValue = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        m_navMeshAgent.destination = m_Rigidbody.position + movement;
+    }
+
+
+    private void Turn()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+}
